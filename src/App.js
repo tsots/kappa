50489537f20abf73cbf8aac72dbc5f0382eae26e
tsots <mosalatsotetsi@gmail.com>
2020-12-16 14:59:45 +0200

    sanity initialized

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Hello world of plenty, today is going to be a great day
+    </div>
+  );
+}
+
+export default App;
---
+++ src/client.js
@@ -0,0 +1,6 @@
+import sanityClient from '@sanity/client'
+
+export default sanityClient({
+  projectId: "7z71366d",
+  dataset: "production"
+})
